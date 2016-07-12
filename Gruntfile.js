3bed51277966031bce1312beed2616daa6045e18
Salah Alomari <s.z.alomari.1987@gmail.com>
2016-07-12 15:38:20 +0400

    Concat uglify jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,14 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      js: {
+        src:['public/client/*.js'],
+        dest:'dist/built.js',
+      },
+      lib: {
+        src: ['public/lib/*.js'],
+        dest: 'dist/lib.js',
+      },
     },
 
     mochaTest: {
@@ -10,7 +18,7 @@ module.exports = function(grunt) {
         options: {
           reporter: 'spec'
         },
-        src: ['test/**/*.js']
+        src: ['test/**/*.js'],
       }
     },
 
@@ -21,45 +29,64 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      dist: {
+        files:{
+          'dist/built.min.js':'dist/built.js',
+          'dist/lib.min.js':'dist/lib.js',
+        },
+      },
     },
-
-    eslint: {
-      target: [
-        // Add list of files to lint here
-      ]
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: ['Gruntfile.js', 'public/client/*.js'],
     },
 
     cssmin: {
+      build: {
+        files:{
+          'dist/application.css': [ 'public/*.css' ],
+        },
+      },
     },
 
     watch: {
       scripts: {
         files: [
           'public/client/**/*.js',
-          'public/lib/**/*.js',
+          'public/lib/**/*.js'
         ],
         tasks: [
+          'jshint',
           'concat',
           'uglify'
         ]
       },
+      build: {
+        files: ['Gruntfile'],
+        tasks: ['jshint']
+      },
       css: {
         files: 'public/*.css',
-        tasks: ['cssmin']
-      }
+        tasks: ['cssmin'],
+      },
     },
 
     shell: {
       prodServer: {
+        command: 'git push heroku master',
+        // command: 'grunt deploy --prod=true'
       }
-    },
+    }
   });
-
+  
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
@@ -96,17 +123,11 @@ module.exports = function(grunt) {
   grunt.registerTask('build', [
   ]);
 
-  grunt.registerTask('upload', function(n) {
-    if (grunt.option('prod')) {
-      // add your production server task here
-    } else {
-      grunt.task.run([ 'server-dev' ]);
-    }
-  });
 
   grunt.registerTask('deploy', [
     // add your deploy tasks here
   ]);
-
+  grunt.registerTask('default', 
+    ['cssmin', 'concat', 'uglify']);
 
 };
